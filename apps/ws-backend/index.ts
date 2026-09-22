5b984128fe2d4efae6f1ea6a38a9a1318cae729a
kinshuk-dewari <kinshukdewariwastaken@gmail.com>
2026-09-22 15:02:05 +0530

    ws onlineLogic

--- apps/ws-backend/index.ts
+++ apps/ws-backend/index.ts
@@ -1,13 +1,19 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import { verify, type JwtPayload } from "jsonwebtoken";
-
+import {prisma} from "@repo/db/client"
 
 const wss = new WebSocketServer({ port: 8080 });
 const JWT_SECRET = process.env.JWT_SECRET ?? "";
 
-const online_users = new Map();
+export type User = {
+    id : string;
+    name : string;
+    ws : WebSocket;
+}
 
-wss.on("connection",(ws,req)=>{
+const onlineUsers:Map<string,User> = new Map();
+
+wss.on("connection",async(ws,req)=>{
 
     const token = req.url?.split("?token=")[1];
 
@@ -15,6 +21,7 @@ wss.on("connection",(ws,req)=>{
         ws.close();
         return;
     }
+
     let decoded;
     try{
         decoded = verify(token, JWT_SECRET) as JwtPayload;
@@ -24,11 +31,33 @@ wss.on("connection",(ws,req)=>{
         return;
     }
 
+    const user = await prisma.user.findUnique({
+        where:{
+            id: decoded.userId
+        }
+    })
+
+    if(!user){
+        ws.close();
+        return;
+    }
+
+    onlineUsers.set(decoded.userId, {
+        name: user.username,
+        ws,
+        id:user.id
+    });
+
+    wss.clients.forEach((ws)=>{
+        ws.send(JSON.stringify({
+            type:"ONLINE_USERS",
+            payload:{
+                users: Array.from(onlineUsers)
+            }
+        }))
+    })
+
     ws.on("message",(event)=>{
         const parsedData=JSON.parse(event.toString());
-
-        if(parsedData.type==="JOIN"){
-            online_users.set()
-        }
     })
 })
