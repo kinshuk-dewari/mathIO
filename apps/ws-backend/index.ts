0fd0f17e6125b716a6489bc9a04b08b6b6c91806
kinshuk-dewari <kinshukdewariwastaken@gmail.com>
2026-09-22 21:22:28 +0530

    game logic

--- apps/ws-backend/index.ts
+++ apps/ws-backend/index.ts
@@ -1,15 +1,18 @@
 import { WebSocketServer, WebSocket } from "ws";
-import { verify, type JwtPayload } from "jsonwebtoken";
 import { prisma } from "@repo/db/client";
+import { verify, type JwtPayload } from "jsonwebtoken";
 import type { Game, Question, User } from "./types";
+import { generateQuestions } from "./utils";
 
 const wss = new WebSocketServer({ port: 8080 });
-const JWT_SECRET = process.env.JWT_SECRET ?? "";
+const JWT_SECRET = process.env.JWT_SECRET!;
 
 const onlineUsers: Map<string, User> = new Map();
-type ExtendedWs = WebSocket & { userId: string };
-
 const games: Map<string, Game> = new Map();
+const currentQuestions: Map<string, number> = new Map();
+const allQuestions: Map<string, Question[]> = new Map();
+
+type ExtendedWs = WebSocket & { userId: string };
 
 wss.on("connection", async (ws: ExtendedWs, req) => {
   const token = req.url?.split("?token=")[1];
@@ -81,7 +84,7 @@ wss.on("connection", async (ws: ExtendedWs, req) => {
             {
               id: user.id,
               name: user.username,
-              ws
+              ws,
             },
           ],
           adminId: user.id,
@@ -105,10 +108,95 @@ wss.on("connection", async (ws: ExtendedWs, req) => {
       const currentGameFetched = games.get(runningGame.id)!;
 
       currentGameFetched.members.push({
-        id:user.id,
+        id: user.id,
         name: user.username,
-        ws
+        ws,
       });
+      const questions = generateQuestions();
+
+      currentGameFetched.questions = questions;
+
+      allQuestions.set(runningGame.id, questions);
+
+      currentGameFetched.status = "RUNNING";
+
+      games.set(currentGameFetched.id, currentGameFetched);
+
+      const firstQuestion = currentGameFetched.questions[0]!;
+
+      const key = `g:${currentGameFetched.id}-u:${user.id}-q:${firstQuestion.id}`;
+
+      console.log("key", key);
+
+      currentQuestions.set(key, 0);
+
+      currentGameFetched.members.forEach((mem) =>
+        mem.ws.send(
+          JSON.stringify({
+            type: "QUESTION",
+            payload: {
+              gameId: runningGame.id,
+              question: firstQuestion,
+            },
+          }),
+        ),
+      );
+    }
+
+    if (parsedData.type == "SUBMIT_ANSWER") {
+      const { gameId, questionId, answer } = parsedData.payload;
+
+      const existingGame = games.get(gameId);
+
+      if (!existingGame) {
+        ws.close();
+        return;
+      }
+
+      const existingQuestion = existingGame.questions.find(
+        (qs) => qs.id == questionId,
+      );
+
+      if (!existingQuestion) {
+        ws.close();
+        return;
+      }
+
+      const filteredAnswers = existingGame.answers.filter((exGm) => {
+        exGm.questionId !== questionId;
+      });
+
+      filteredAnswers.push({
+        id: crypto.randomUUID(),
+        answer,
+        questionId,
+      });
+
+      if (answer !== existingQuestion.answer) {
+        return;
+      }
+
+      const key = `g:${existingGame.id}-u:${user.id}-q:${existingQuestion.id}`;
+      console.log("key", key);
+      const currentQuestionIndex = currentQuestions.get(key) ?? 0;
+      const storedQuestions = allQuestions.get(existingGame.id)!;
+
+      console.log("storedQuestions", storedQuestions);
+
+      const nextQuestion = storedQuestions[currentQuestionIndex + 1];
+      console.log("next question", nextQuestion);
+      console.log("index", currentQuestionIndex);
+      currentQuestions.set(key, currentQuestionIndex + 1);
+
+      ws.send(
+        JSON.stringify({
+          type: "QUESTION",
+          payload: {
+            gameId: existingGame.id,
+            question: nextQuestion,
+          },
+        }),
+      );
     }
   });
 });
